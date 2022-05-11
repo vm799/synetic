83be89b49bb4e538c0e74b5671cb157d866d4379
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-11 18:51:27 +0100

    class errors

--- synetic-ss/src/components/ImageSlider2/ImageSlider2.jsx
+++ synetic-ss/src/components/ImageSlider2/ImageSlider2.jsx
@@ -4,9 +4,9 @@ import NFTimage from "../../images/10.png"
 
 export default function ImageSlider2() {
     return(
-<div class="image-slider2">
-    <div class="marquee_image2">
-  <div class="marquee_image2-content">
+<div className="image-slider2">
+    <div className="marquee_image2">
+  <div className="marquee_image2-content">
   <img src= { NFTimage } alt="new-synetic_souls nft"></img>
   <img src= { NFTimage } alt="new-synetic_souls nft"></img>
   <img src= { NFTimage } alt="new-synetic_souls nft"></img>
