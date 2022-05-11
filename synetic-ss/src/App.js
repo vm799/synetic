0f6f9492e36366be882862dd203d7c72f8dd9a0f
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-11 20:03:55 +0100

    restructure app

--- synetic-ss/src/App.js
+++ synetic-ss/src/App.js
@@ -1,33 +1,12 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar"
-import Team from "./components/Team/Team"
-// import Syneverse from "./components/Syneverse/Syneverse"
-import About from "./components/About/About"
-import NFTgame from "./components/NFTgame/NFTgame";
-import Hero from "./components/Hero/Hero";
-import Roadmap from "./components/Roadmap/Roadmap";
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
-import ImageSlider from "./components/ImageSlider/ImageSlider";
-import TextSlider from "./components/TextSlider/TextSlider";
-import ImageSlider2 from "./components/ImageSlider2/ImageSlider2";
+import { BrowserRouter } from "react-router-dom";
+import Views from "./Views";
 
 function App() {
   return (
-    <div className="App">
-     <Navbar />
-     <Hero />
-     <ImageSlider />
-     <ImageSlider2 />
-     <Team />
-     <TextSlider />
-     {/* <Syneverse /> */}
-     <NFTgame />
-     <About />
-     <Roadmap />
-     <Contact />
-     <Footer />
-    </div>
+    <BrowserRouter>
+    <Views />
+    </BrowserRouter>
   )
 }
 
