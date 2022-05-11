7f5e85fc7814a788adf3f4c51fbed8341bb000a1
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-11 20:50:22 +0100

    undo browser react

--- synetic-ss/src/App.js
+++ synetic-ss/src/App.js
@@ -1,13 +1,36 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Views from "./Views";
+import Navbar from "./components/Navbar/Navbar"
+import Team from "./components/Team/Team"
+// import Syneverse from "./components/Syneverse/Syneverse"
+import About from "./components/About/About"
+import NFTgame from "./components/NFTgame/NFTgame";
+import Hero from "./components/Hero/Hero";
+import Roadmap from "./components/Roadmap/Roadmap";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import ImageSlider from "./components/ImageSlider/ImageSlider";
+import TextSlider from "./components/TextSlider/TextSlider";
+import ImageSlider2 from "./components/ImageSlider2/ImageSlider2";
 
 function App() {
-  return (
-    <BrowserRouter>
-    <Views />
-    </BrowserRouter>
-  )
+  return(
+    <div className="App">
+          
+       <Navbar />
+         <Hero />
+         <ImageSlider />
+         <ImageSlider2 />
+         <Team />
+         <TextSlider />
+         {/* <Syneverse /> */}
+         <NFTgame />
+         <About />
+         <Roadmap />
+         <Contact />
+         <Footer /> 
+    
+    </div>
+        )
 }
 
 export default App;
