f7df34fd0e7672751bd58165aac5b1596abf632a
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-11 16:00:38 +0100

    instal react-router

--- synetic-ss/src/components/Navbar/Navbar.jsx
+++ synetic-ss/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-
 import "../Navbar/Navbar.css"
+import { MdClose } from "react-icons/md"
+import { FiMenu } from "react-icons/fi"
+import { NavLink } from 'react-router-dom'
 
 export default function Navbar(){
 
@@ -10,13 +12,32 @@ const handleToggle = ()=> {
 setNavbarOpen(prev => !prev)
 }
 
+
+
+const closeMenu = () => {
+    setNavbarOpen(false)
+}
+
+
     return (
         <nav className= "nav">
+            <NavLink to={link.path}
+activeClassName="active-link"
+onClick= { () => closeMenu() }
+exact
+>
+</NavLink>
+
 <a href="#" className= "logo" >
     SYNETIC
 </a>
 
-<button onClick={ handleToggle }> {navbarOpen? "Close" : "Open"}</button>
+<button onClick={ handleToggle }> {navbarOpen? 
+(<MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+  ) : (
+    <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+  )}</button>
+
 {/* <div className="page-links">
     {/* <p>About</p> */}
     {/* <p>About</p>
