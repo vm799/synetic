718ec3dd538c900891d78eafe65cbe3730b6314a
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-14 14:46:17 +0100

    xursor

--- synetic-ss/src/App.js
+++ synetic-ss/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext} from "react";
 import Navbar from "./components/Navbar/Navbar"
 import Team from "./components/Team/Team"
 // import Syneverse from "./components/Syneverse/Syneverse"
@@ -14,9 +14,10 @@ import ImageSlider2 from "./components/ImageSlider2/ImageSlider2";
 import Oasis from "./components/Oasis/Oasis";
 import DotRing from "./components/DotRing/DotRing";
 import "./App.css";
+import { MouseContext } from "./context/mouse-context";
 
 function App() {
-  
+  const { cursorType, cursorChangeHandler } = useContext(MouseContext);
   return(
     <div className="App">
       <DotRing />
@@ -25,10 +26,9 @@ function App() {
           onMouseEnter={() => cursorChangeHandler("hovered")}
           onMouseLeave={() => cursorChangeHandler("")}
         >
-          <h1>Hover over me</h1>
+        
         </div>
       </div>
-      <div className="container" style={{ background: "peachpuff" }}></div>
   
 
        <Navbar />
