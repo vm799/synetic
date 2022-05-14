ddbcebfee1b4879a5b1abad266947f6e8f029c14
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-14 12:07:05 +0100

    oasis block

--- synetic-ss/src/components/Oasis/Oasis.jsx
+++ synetic-ss/src/components/Oasis/Oasis.jsx
@@ -9,20 +9,26 @@ export default function Oasis(){
     <section class="about-oasis" id="about">
     <h2 class="section__title section__title--about">The Oasis</h2>
     <p class="section__subtitle section__subtitle--about">
-        The oasis is a Photoralistic, 
-        Virtual re-creation of reality where you can live a second life without the constraints of modern day reality. </p>
+       Photorealistic, 
+        virtual  reality. </p>
+        <br />
+        <p class="section__subtitle section__subtitle--about">
+      Live an alternate life without the constraints of modern day reality. </p>
     
     <div class="about-oasis__body">
-        <p>Built with the blockchain, players will be able to build, own and monetise thier experiances
-through Virtual in game real-estate ownership.This concept opens up acorporate playing field 
-for entrepeuners, the focus is to create an ecenomy purely built upon capitilism where new 
-jobs and corporations can emurge purely from the foundation of our game.</p>
-        <p>Once spawning into the open world planet, players will have acces to many different planets and worlds where you can 
-fight other players for coins, join virtual metings with co-workers,walk into a vegas style casino, even fly a fighter-jet and more...
-all from the comfort of your own home !</p>
+        <p>Built with the blockchain. Build, own and monetise your experiences
+through real-estate ownership.  Become an entrepreneur, join the corporate playing field.
+Built on a capitalist model, new 
+jobs and corporations can flourish organically from the foundation of our game.</p>
+
+<img src="https://images.unsplash.com/photo-1577936220068-923b4be596cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60" alt="Jane leaning against a bus" class="about-me__img" />
+
+        <p>Spawn into the open world planet and have access to a multiverse of many different planets and worlds where you can 
+fight other players for coins, join virtual metings with co-workers, walk into a vegas style casino or even fly a fighter-jet.
+All from the comfort of your own home !</p>
     </div>
     
-    <img src="https://images.unsplash.com/photo-1577936220068-923b4be596cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60" alt="Jane leaning against a bus" class="about-me__img" />
+    <img src="https://images.unsplash.com/photo-1484669646614-0c9cd3d4a453?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZ1dHVyaXN0aWMlMjBjaXR5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="sunset over dystopia" class="about-me__img" />
  </section>
     
        
