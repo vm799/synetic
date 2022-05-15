7db9697b9e50fa664f4b13b1b83abd2d815cff51
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-15 09:54:25 +0100

    oasis main boy text

--- synetic-ss/src/components/Oasis/Oasis.jsx
+++ synetic-ss/src/components/Oasis/Oasis.jsx
@@ -12,7 +12,7 @@ export default function Oasis(){
            <p class="section__subtitle section__subtitle--about">Designer & developer based out of NYC</p>
            
            <div class="about-me__body">
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+               <p className="highlight">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
            </div>
            
