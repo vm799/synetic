d52536a9700ff2078bbd1f678a76a4e08c712ef7
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-05-24 17:19:15 +0100

    index.css

--- synetic-ss/src/components/About/About.jsx
+++ synetic-ss/src/components/About/About.jsx
@@ -15,14 +15,16 @@ export default function About(){
     return(
         <div id="about-main" 
         className="about"
-        style={{ transform: `translateY(-${offsetY * 0.5}px)` }}>
+        style={{ transform: `translateY(-${offsetY * 0.5}px)` }}
+        >
      
-        <div className="about-image"   >
+        <div className="about-image" >
         <h1>ABOUT</h1>
         <img width="600" height="400" src="https://images.unsplash.com/photo-1573767291321-c0af2eaf5266?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8ZnV0dXJpc3RpYyUyMGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60" alt="" />
         </div>
 
-        <div className="about-text">
+        <div className="about-text"
+        >
             
             <p className="highlight-text"> A new open world multi-planetary system created with endless possibilities for escape, on the blockchain. </p>
             
